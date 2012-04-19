d0ec808911f21a293b773df55414d5f678e929a8
Álvaro Villalba Navarro <vn.alvaro@gmail.com>
2012-04-19 02:45:31 +0200

    minor fix again. woooohoo, someday I'll be very ashamed for this dirty branch.

--- src/transition.c
+++ src/transition.c
@@ -120,7 +120,9 @@ end:
 
 Transition* trans_clist_free(struct reactor_d *reactor, Transition *trans){
     Transition *ret;
-    reactor_slist_foreach(trans->enrequisites, en_remove_one_curr_trans, trans);
+    if(trans == NULL) return NULL;
+    if(trans->enrequisites != NULL)
+        reactor_slist_foreach(trans->enrequisites, en_remove_one_curr_trans, trans);
     while(trans->enrequisites != NULL){
         en_unref(reactor, trans->enrequisites->data);
         trans->enrequisites = reactor_slist_delete_link(trans->enrequisites, trans->enrequisites);
